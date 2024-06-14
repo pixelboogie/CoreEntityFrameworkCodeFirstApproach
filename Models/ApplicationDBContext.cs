cf23dc5ce71054a1aca905a61814a083eb525415
pixelboogie <justamovie@yahoo.com>
2024-06-14 11:18:21 -0500

    Imported the Core packages. stopped on conifuring ApplicationDBContext.

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreEntityFrameworkCodeFirstApproach.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            // She got server from SQL server manager???
+            optionsBuilder.UseSqlServer("Server=.;Database=CodeFirstDB;Integrated Securty=true TrustServerCertificate = true");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreEntityFrameworkCodeFirstApproach.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public double CustomerAmount { get; set; }
+    }
+}
